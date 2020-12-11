a02b23cbf94bf66df120e197585506d7e948a918
confucianzuoyuan <zuoyuantc@126.com>
2020-12-11 15:30:13 +0800

    update

--- flink-runtime/src/main/java/org/apache/flink/runtime/clusterframework/BootstrapTools.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/clusterframework/BootstrapTools.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.flink.runtime.clusterframework;
 
 import akka.actor.ActorSystem;
@@ -37,23 +19,6 @@ import static org.apache.flink.configuration.ConfigOptions.key;
  * Actor Systems used to run the JobManager and TaskManager actors.
  */
 public class BootstrapTools {
-	/**
-	 * Internal option which says if default value is used for {@link CoreOptions#TMP_DIRS}.
-	 */
-	private static final ConfigOption<Boolean> USE_LOCAL_DEFAULT_TMP_DIRS = key("internal.io.tmpdirs.use-local-default")
-		.defaultValue(false);
-
-	private static final Logger LOG = LoggerFactory.getLogger(BootstrapTools.class);
-
-	private static final Escaper UNIX_SINGLE_QUOTE_ESCAPER = Escapers.builder()
-		.addEscape('\'', "'\\''")
-		.build();
-
-	private static final Escaper WINDOWS_DOUBLE_QUOTE_ESCAPER = Escapers.builder()
-		.addEscape('"', "\\\"")
-		.addEscape('^', "\"^^\"")
-		.build();
-
 	public static ActorSystem startLocalActorSystem(
 		Configuration configuration,
 		String actorSystemName,
@@ -81,13 +46,6 @@ public class BootstrapTools {
 		}
 	}
 
-	/**
-	 * Starts an Actor System with given Akka config.
-	 * @param akkaConfig Config of the started ActorSystem.
-	 * @param actorSystemName Name of the started ActorSystem.
-	 * @param logger The logger to output log information.
-	 * @return The ActorSystem which has been started.
-	 */
 	private static ActorSystem startActorSystem(Config akkaConfig, String actorSystemName, Logger logger) {
 		logger.debug("Using akka configuration\n {}", akkaConfig);
 		ActorSystem actorSystem = AkkaUtils.createActorSystem(actorSystemName, akkaConfig);
@@ -95,10 +53,6 @@ public class BootstrapTools {
 		logger.info("Actor system started at {}", AkkaUtils.getAddress(actorSystem));
 		return actorSystem;
 	}
-
-	private static final String DYNAMIC_PROPERTIES_OPT = "D";
-
-	// ------------------------------------------------------------------------
 
 	/** Private constructor to prevent instantiation. */
 	private BootstrapTools() {}
--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/environment/StreamExecutionEnvironment.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/environment/StreamExecutionEnvironment.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.flink.streaming.api.environment;
 
 import org.apache.flink.annotation.Internal;
@@ -40,8 +23,6 @@ import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.graph.StreamGraph;
 import org.apache.flink.streaming.api.graph.StreamGraphGenerator;
 import org.apache.flink.streaming.api.operators.StreamSource;
-import org.apache.flink.util.ExceptionUtils;
-import org.apache.flink.util.FlinkException;
 import org.apache.flink.util.Preconditions;
 
 import java.io.IOException;
@@ -50,7 +31,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import static org.apache.flink.util.Preconditions.checkNotNull;
 
@@ -104,10 +84,6 @@ public class StreamExecutionEnvironment {
 		// execution. We're keeping this to appease the binary compatibiliy checks.
 	}
 
-	/**
-	 * Creates a new {@link StreamExecutionEnvironment} that will use the given {@link
-	 * Configuration} to configure the {@link PipelineExecutor}.
-	 */
 	@PublicEvolving
 	public StreamExecutionEnvironment(final Configuration configuration) {
 		this(configuration, null);
