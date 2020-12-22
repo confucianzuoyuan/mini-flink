c3b56dd84006d3c2e489b3f1483c2453f6248567
confucianzuoyuan <zuoyuantc@126.com>
2020-12-22 18:32:41 +0800

    update

--- flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/HistoryServerArchivist.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/HistoryServerArchivist.java
@@ -1,28 +1,10 @@
 package org.apache.flink.runtime.dispatcher;
 
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.configuration.JobManagerOptions;
-import org.apache.flink.core.fs.Path;
-import org.apache.flink.runtime.executiongraph.AccessExecutionGraph;
-import org.apache.flink.runtime.messages.Acknowledge;
-import org.apache.flink.runtime.webmonitor.WebMonitorUtils;
-
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 public interface HistoryServerArchivist {
-
-	CompletableFuture<Acknowledge> archiveExecutionGraph(AccessExecutionGraph executionGraph);
-
 	static HistoryServerArchivist createHistoryServerArchivist(Configuration configuration, Executor ioExecutor) {
-		final String configuredArchivePath = configuration.getString(JobManagerOptions.ARCHIVE_DIR);
-
-		if (configuredArchivePath != null) {
-			final Path archivePath = WebMonitorUtils.validateAndNormalizeUri(new Path(configuredArchivePath).toUri());
-
-			return new JsonResponseHistoryServerArchivist(archivePath, ioExecutor);
-		} else {
-			return VoidHistoryServerArchivist.INSTANCE;
-		}
+		return VoidHistoryServerArchivist.INSTANCE;
 	}
 }
--- flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/VoidHistoryServerArchivist.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/VoidHistoryServerArchivist.java
@@ -18,19 +18,9 @@
 
 package org.apache.flink.runtime.dispatcher;
 
-import org.apache.flink.runtime.executiongraph.AccessExecutionGraph;
-import org.apache.flink.runtime.messages.Acknowledge;
-
-import java.util.concurrent.CompletableFuture;
-
 /**
  * No-op implementation of the {@link HistoryServerArchivist}.
  */
 public enum VoidHistoryServerArchivist implements HistoryServerArchivist {
 	INSTANCE;
-
-	@Override
-	public CompletableFuture<Acknowledge> archiveExecutionGraph(AccessExecutionGraph executionGraph) {
-		return CompletableFuture.completedFuture(Acknowledge.get());
-	}
 }
